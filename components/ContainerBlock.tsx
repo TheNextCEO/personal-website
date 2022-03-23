d3ab98dddb925984f19b6493dfd20c862cb24c99
Benji <charles.benji33@gmail.com>
2022-03-22 22:28:41 -0400

    og:site_name

--- components/ContainerBlock.tsx
+++ components/ContainerBlock.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Flex, HStack } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React from "react";
@@ -32,7 +32,7 @@ export default function ContainerBlock({
           href={`https://benjicharles.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Benji Charles" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
